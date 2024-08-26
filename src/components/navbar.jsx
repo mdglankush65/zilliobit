f6747cd0dae815c3e8d9917202f71acfa766fa47
Ankush Mudgil <101817825+mdglankush65@users.noreply.github.com>
2024-08-26 10:43:31 +0530

    responsive

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,38 +1,51 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
-
-// eslint-disable-next-line react/prop-types
-const Navbar = ({title,menuList}) => {
+// eslint-disable-next-line
+const Navbar = ({ title, menuList }) => {
     const navigate = useNavigate();
+
     return (
         <nav className="navbar">
             <div className="nav-left">
                 <div className="logo-nav">Your logo</div>
-                {
-                    title &&
-                        <>
-                            <div className="topic-nav">Cyber Saftey</div>
-                            <div className="title-nav">{title} </div>
-                        </>
-                }
+                {window.innerWidth>=768 && title && (
+                    <>
+                        <div className="topic-nav">Cyber Safety</div>
+                        <div className="title-nav">{title}</div>
+                    </>
+                )}
             </div>
 
             <div className="nav-right">
-                {menuList && <div onClick={()=>navigate('/menu')}>
-                    <div className="menu-logo"></div>
-                    <div>Menu</div>
-                </div>}
-                <div>
-                    <div className="mute-logo"></div>
-                    <div>Mute</div>
-                </div>
-                <div onClick={()=>navigate('/')}>
-                    <div className="exit-logo"></div>
-                    <div>Exit</div>
-                </div>
+                { window.innerWidth<768 ? (
+                    <div>
+                        <div className="hamburger-menu">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+                ) : (
+                    <>
+                        {menuList && (
+                            <div onClick={() => navigate('/menu')}>
+                                <div className="menu-logo"></div>
+                                <div>Menu</div>
+                            </div>
+                        )}
+                        <div>
+                            <div className="mute-logo"></div>
+                            <div>Mute</div>
+                        </div>
+                        <div onClick={() => navigate('/')}>
+                            <div className="exit-logo"></div>
+                            <div>Exit</div>
+                        </div>
+                    </>
+                )}
             </div>
         </nav>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
